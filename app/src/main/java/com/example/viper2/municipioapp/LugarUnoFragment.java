88e4a4ab5a6eee4d6bb1414e144aac3179907146
DavidQM <alexis.quintero@udea.edu.co>
2017-03-23 09:06:11 -0500

    Version - Practica 3 - Generacion de Fragments

--- app/src/main/java/com/example/viper2/municipioapp/LugarUnoFragment.java
+++ app/src/main/java/com/example/viper2/municipioapp/LugarUnoFragment.java
@@ -1,11 +1,11 @@
 package com.example.viper2.municipioapp;
 
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
 
 
 /**
