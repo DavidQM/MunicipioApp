7e611c3d55d15c7550700d3458998cd17e77602e
DavidQM <alexis.quintero@udea.edu.co>
2017-04-10 20:09:12 -0500

    practica 4 - mejora del splashactivity - pendiente los demas Drawer

--- app/src/main/java/com/example/viper2/municipioapp/SplashActivity.java
+++ app/src/main/java/com/example/viper2/municipioapp/SplashActivity.java
@@ -1,9 +1,11 @@
 package com.example.viper2.municipioapp;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,6 +17,11 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_splash);
 
         Thread timerThread = new Thread(){
